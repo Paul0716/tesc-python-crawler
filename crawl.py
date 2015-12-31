875c6a0be8d0a2edca3fc41c7455000b60732178
Paul0716 <justlove0714@hotmail.com>
2016-01-01 02:09:26 +0800

    daily update.

--- crawl.py
+++ crawl.py
@@ -1,14 +1,20 @@
 # -*- coding: utf-8 -*-
 import csv
 import urllib
-
+import datetime
 from functions import *
+
 
 # 從 stocknumber.csv 中讀出要爬的股票清單
 stock_list = []
 stock_list = get_stock_list('stocknumber.csv')
+today = datetime.datetime.today()
+start_from = today - datetime.timedelta(days=4*365)
 
-get_single_page('0050','2015','12')
+
+
+
+# get_single_page('0050','2015','12')
 
 
 
--- functions.py
+++ functions.py
@@ -2,8 +2,6 @@
 import csv 
 import requests
 from pyquery import PyQuery as pq
-from lxml import etree
-
 
 def get_stock_list(filename):
 	stock_id_list = []
@@ -13,38 +11,52 @@ def get_stock_list(filename):
 	return stock_id_list
 
 def get_single_page(stock_id, year, month):
-	with open('data/%s.csv'%stock_id, 'a') as f:
 
+	path = 'data/%s.csv'%stock_id
+
+	csv_data = read_single_file(path)
+
+	with open(path, 'ab') as f:			
 		cw = csv.writer(f)
 		page = requests.get('http://www.twse.com.tw/ch/trading/exchange/STOCK_DAY/genpage/Report'+year+month+'/'+year+month+'_F3_1_8_'+stock_id+'.php?STK_NO='+stock_id+'&myear='+year+'&mmon='+month)
-
 		if page.status_code is 200:
 			d = pq(page.text)
 			row = d('tr.basic2')
 			for key,value in enumerate(row):
-
 				if key is not 0:
-					data = d(value).text().split(' ')
-					print key,d(value).text().replace(' ',',')
-					cw.writerow(data)
+					data = d(value).text().strip('"').split(' ')
+					
+					if data not in csv_data:
+						cw.writerow(data)
 			f.close();
 		else:
-			pass
-# 錯誤輸出檔案
+			print record_error_log("發生錯誤: status_code %s" % page.status_code)
 
+# 錯誤輸出檔案
 def record_error_log(msg):
 	error_log = open('error.log', 'ab')
+	error_log.write(msg+'/r/n')
+	error_log.close()
+	return msg
 
+def read_single_file(csvfile):
+	data_list = []
+	with open(csvfile, 'r') as f:
+		x = csv.reader(f, delimiter=',', quotechar='"')
+		for row in x:
+			data_list.append(row)
+		return data_list
 
-
-
-
-
-
-
-
-
+def tw_time_converter(*args, **kwargs):
+	target_date = args[0]
+	year = int(target_date[0])
+	month = int(target_date[1])
+	day = int(target_date[2])
+	return datetime.date(year+1911, month, day+1).strftime("%Y-%m-%d")
 	
-
-
+def overwrite_csv_line(*args, **kwargs):
+	with open(kwargs['csvfile'], 'w') as b:
+		writer = csv.writer(b)
+		print kwargs["data"]
+		writer.writerow(kwargs["data"])
 
